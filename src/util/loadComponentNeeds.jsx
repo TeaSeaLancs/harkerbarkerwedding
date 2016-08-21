9cf647850d14e443f61474ab04828d69832ac757
Matt Harker <matt@theflux.co.uk>
2016-08-21 09:47:32 +0100

    Flatten out needs gathering

--- src/util/loadComponentNeeds.jsx
+++ src/util/loadComponentNeeds.jsx
@@ -1,24 +1,12 @@
-const loadNeeds = (store, component) => {
-    if (!component.needs) {
-        return null;
-    }
-    
-    const needs = component.needs();
-    if (!needs || !needs.length) {
-        return null;
-    }
-    
-    return Promise.all(needs.map(need => {
-        console.log("Loading need ", need);
-        return store.dispatch(need());
-    }));
-}
-
 export default (store, components) => {
     if (!Array.isArray(components)) {
         components = [components];
     }
-    console.log("Components", components);
     
-    return Promise.all(components.map(component => loadNeeds(store, component)));
+    const needs = components.reduce((allNeeds, component) => {
+        allNeeds.push.apply(allNeeds, (component.needs && component.needs()) || []);
+        return allNeeds;
+    }, []);
+    
+    return Promise.all(needs.map(need => store.dispatch(need())));
 }
