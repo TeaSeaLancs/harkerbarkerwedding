af296429a610fecaa1a1cef4fb7535b5fee1667f
Matt Harker <matt@theflux.co.uk>
2016-09-18 16:54:38 +0100

    Je suis une muppet

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ require('isomorphic-fetch');
 require('./src/util/css');
 
 const http = require('http');
-//const https = require('https');
+const https = require('https');
 const app = require('koa')();
 const serve = require('koa-static');
 const bodyparser = require('koa-better-body');
 
-//const le = require('./src/util/letsencrypt');
+const le = require('./src/util/letsencrypt');
 
 app.keys = ['koa-tutorial'];
 app.use(bodyparser());
@@ -19,8 +19,15 @@ app.use(require('./src/admin/'));
 
 require('./src/util/requirements').then(() => {
     
-    http.createServer(app.callback()).listen(9000, () => {
-       console.log("Server up"); 
-    });
+    if (process.env.NODE_ENV === 'development') {
+        http.createServer(app.callback()).listen(9000, () => {
+            console.log("Dev server up");
+        });
+    } else {
+        const server = https.createServer(le.httpsOptions, le.middleware(app.callback()));
+        server.listen(9443, () => {
+            console.log(`Production server up on 9443`);
+        });
+    }
     
 }).catch(err => console.error(err));
